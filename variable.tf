9887a9d37e487d46209bbae1061b80d1b337ab36
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2024-07-30 13:06:23 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable ami {
+        description = "ami of the server"
+
+}
+
+variable instance_type {
+        description = "instance type of the server"
+
+}
+
+variable aws_region {
+        description = "region where we create resource"
+        default = "us-east-1"
+
+}
